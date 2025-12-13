b0558ea3355289b517316d43bcc0dc39d47c0d87
Pinja Savenius <pinja.savenius@aalto.fi>
2025-12-13 19:25:31 +0200

    Lisätty gamecontrolleriin edit ja delete toiminnot

--- controllers/games_controller.py
+++ controllers/games_controller.py
@@ -37,3 +37,36 @@ def add_game():
         return redirect("/games")
     # Renders the template for adding games if request method was GET
     return render_template("add_game.html")
+
+# Route for deleting game
+@games_controller.route("/delete_game/<int:id>", methods=["POST"])
+def delete_game(id):
+    connection = get_db_connection()
+    connection.execute("DELETE FROM games WHERE id = ?", (id,))
+    connection.commit()
+    connection.close()
+    return redirect("/games")
+
+# Route for editing player
+@games_controller.route("/edit_player/<int:id>", methods=["GET","POST"])
+def edit_game(id):
+    connection = get_db_connection()
+    if request.method == "POST": # Editing data in the database if request method was POST
+        name = request.form["game_name"]
+        genre = request.form["genre"]
+        rating = request.form["rating"]
+        min_players = request.form["min_players"]
+        max_players = request.form["max_players"]
+        last_updated = request.form["last_updated"]
+        connection.execute("UPDATE games SET name = ?, genre = ?, rating = ?, min_players = ?, max_players = ?, last_updated = ? WHERE id = ?",
+                           (name, genre, rating, min_players, max_players, last_updated, id))
+        connection.commit()
+        connection.close()
+        return redirect("/games")
+
+    game_to_be_edited = connection.execute("SELECT * FROM games WHERE id = ?", (id,)).fetchone() # Fetches only one game with the given id, if that id doesn't exist, then this method returns None
+    connection.close()
+    if game_to_be_edited is None: # Checks if the game was found
+        return Response("Game not found", status=404) # This status code indicates that server cannot find the requested resource, that is when the selected game didn't exist.
+    game_object = Game(game_to_be_edited) # Game was found and this changes the fetched data to a game object
+    return render_template("edit_game.html", player = game_object) # Selected player exists and will be forwarded to view
