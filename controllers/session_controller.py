15fdeafa537f5bce6e0b8d1bc9dc3258ac441f30
Pinja Savenius <pinja.savenius@aalto.fi>
2025-12-14 15:28:32 +0200

    Lisätty add session toiminto ja näkymä

--- controllers/session_controller.py
+++ controllers/session_controller.py
@@ -5,6 +5,8 @@ from models.session import Session
 
 session_controller = Blueprint("session_controller", __name__)
 
+# LISTING SESSIONS
+###########################################################################################
 # Route for listing sessions
 @session_controller.route("/sessions") # Method is GET by default unless stated otherwise
 def get_sessions():
@@ -52,3 +54,70 @@ def get_sessions():
     connection.close()
 
     return render_template("sessions.html", sessions=session_objects_list)
+
+#################################################################################################
+# ADDING SESSION
+@session_controller.route("/add_session", methods=["GET","POST"])
+def add_session():
+    connection = get_db_connection()
+    if request.method == "POST":
+        # Collects data from forms in add_session view
+        game_id = request.form["game_id"]
+        duration = request.form["duration_minutes"]
+        date_played = request.form["date_played"]
+        participants = request.form.getlist("participants")
+        winners = request.form.getlist("winners")
+        # New session to sessions-board
+        cursor = connection.execute("""
+            INSERT INTO sessions (game_id, duration_minutes, date_played) VALUES (?,?,?)
+        """, (game_id,duration,date_played))
+
+        new_session_id = cursor.lastrowid
+
+        # New session's participant(s) to session_players-board
+        for player_id in participants:
+            connection.execute("""
+                INSERT INTO session_players (session_id, player_id) VALUES (?,?)
+        """, (new_session_id, player_id))
+
+        # New session's winner(s)
+        for winner_id in winners:
+            connection.execute("""
+                INSERT INTO session_winners (session_id, player_id) VALUES (?,?)    
+        """, (new_session_id, winner_id))
+
+        connection.commit()
+        connection.close()
+        return  redirect("/sessions")
+
+    # If request method was GET
+    games_list = connection.execute("SELECT * FROM games").fetchall()
+    players_list = connection.execute("SELECT * FROM players").fetchall()
+    connection.close()
+    return render_template("add_session.html", games=games_list, players=players_list)
+######################################################################################################
+# DELETING SESSION
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- models/session.py
+++ models/session.py
@@ -2,7 +2,7 @@ class Session:
     def __init__(self, row, game_name=None, participants=None, winners=None):
         self.id = row["id"]
         self.game_id = row["game_id"]
-        self.duration = row["duration"]
+        self.duration_minutes = row["duration_minutes"]
         self.date_played = row["date_played"]
 
         self.game_name = game_name
